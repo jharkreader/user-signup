69ba50e1e21287205290b971f135aea549f907dd
JH <jennifer@substantial.com>
2017-06-16 12:45:05 -0700

    Removed static structure, completed input validation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, redirect, render_template, send_from_directory
+from flask import Flask, request, redirect, render_template
 import cgi
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 app.config['DEBUG'] = True 
 
 
@@ -10,13 +10,70 @@ def index():
 
     return render_template('index.html')
 
-@app.route("/send", methods=["POST"])
+
+@app.route("/", methods=["POST"])
+def validate_signup():
+    username = request.form['username']
+    pw = request.form['password']
+    v_pw = request.form['v_password']
+    email = request.form['email']
+
+    username_error = ''
+    pw_error = ''
+    v_pw_error = ''
+    email_error = ''
+
+    if username == "":
+        username_error = "Please enter a username."
+    elif len(username) < 3 or len(username) > 20:
+        username_error = "User name must be greater than 3 but less than 20 characters."
+    elif " " in username:
+        username_error = "User name cannot contain spaces."
+    else:
+        pass        
+
+    if pw == "":
+        pw_error = "Please enter a password."
+    elif len(pw) < 3 or len(pw) > 20:
+        pw_error = "Password must be greater than 3 but less than 20 characters."
+        pw = ""
+        v_pw = ""
+    elif " " in pw:
+        pw_error = "Password cannot contain spaces."
+        pw = ""
+        v_pw = ""
+    elif pw != v_pw:
+        v_pw_error = "Passwords must match."
+        pw_error = "Passwords must match."
+        v_pw = ""
+        pw = ""    
+    else:
+        pass        
+
+    if email != "":
+        if "@" not in email or "." not in email or len(email) < 3 or len(email) > 20 or " " in email:
+            email_error = "Please enter a valid email address."
+    else:
+        pass
+
+
+    if not username_error and not pw_error and not v_pw_error and not email_error:
+        return redirect('/valid-signup?username={0}'.format(username))
+
+    else:
+        return render_template('index.html', 
+        username_error=username_error, 
+        pw_error=pw_error, 
+        v_pw_error=v_pw_error, 
+        email_error=email_error, 
+        username=username, 
+        email=email)    
+
+
+@app.route("/valid-signup")
 def send():
-    username = request.form["username"]
+    username = request.args.get("username")
     return render_template('welcome.html', username=username)
 
-@app.route('/static')
-def send_css(/static):
-    return send_from_directory('stylesheet.css', /static)
 
 app.run()    
